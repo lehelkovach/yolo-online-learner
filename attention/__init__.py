132a33fffb621a1f2524e6de8c00c464bced5b99
Lehel John Kovach <lehel@g.ucla.edu>
2026-07-31 01:55:03 -0400

    Add Stage 2 attention package

---
+++ attention/__init__.py
@@ -0,0 +1,5 @@
+"""Attention routing for the serial BBP stream."""
+
+from attention.scheduler import AttentionScheduler, AttentionSelection
+
+__all__ = ["AttentionScheduler", "AttentionSelection"]
